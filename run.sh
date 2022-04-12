a1e0748fa8f1d9edd0a4c58b0356e28459c83691
Anton Pirker <anton.pirker@sentry.io>
2022-04-12 17:17:21 +0200

    Fixed filename and setting up environment in run.sh

--- run.sh
+++ run.sh
@@ -2,5 +2,12 @@
 
 set -e
 
+# Create Python virtual environment
+python -m venv .venv
+source .venv/bin/activate
 
+# Install Python requirements
+pip install -r requirements.txt
+
+# Run Flask app
 FLASK_APP=app flask run  
